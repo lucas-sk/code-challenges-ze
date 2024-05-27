313564f98ee243cbaed2c1e5d1af4d634184b6cc
lusk <lucas78654321@outlook.com>
2024-05-27 20:54:03 -0300

    test: add E2E test for partner registration

--- src/http/controllers/partner/register.spec.ts
+++ src/http/controllers/partner/register.spec.ts
@@ -1,5 +1,54 @@
-import { describe, it } from 'vitest'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-describe('Register Partner', () => {
-  it('ok', () => {})
+import { app } from '@/app'
+
+describe('Register Partner (E2E)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register a partner', async () => {
+    const response = await request(app.server)
+      .post('/partners')
+      .send({
+        tradingName: 'Adega Osasco',
+        ownerName: 'Joao Silva',
+        document: '01.2134.567/00201-891',
+        coverageArea: {
+          type: 'MultiPolygon',
+          coordinates: [
+            [
+              [
+                [-49.32291, -25.4398],
+                [-49.29751, -25.43377],
+                [-49.25585, -25.4169],
+                [-49.25524, -25.40981],
+                [-49.25761, -25.40403],
+                [-49.25524, -25.39787],
+                [-49.26005, -25.39178],
+                [-49.26078, -25.3819],
+                [-49.26267, -25.37348],
+                [-49.25952, -25.37003],
+                [-49.25971, -25.36597],
+                [-49.26301, -25.35774],
+                [-49.26468, -25.34742],
+                [-49.30623, -25.35119],
+                [-49.32291, -25.4398],
+              ],
+            ],
+          ],
+        },
+        address: {
+          type: 'Point',
+          coordinates: [-49.283756, -25.401233],
+        },
+      })
+
+    expect(response.statusCode).toBe(201)
+  })
 })
