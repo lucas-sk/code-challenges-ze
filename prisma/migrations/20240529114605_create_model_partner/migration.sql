88d06f72e97589588aa5906d6a8941ea7dcf328a
lusk <lucas78654321@outlook.com>
2024-05-29 12:01:59 -0300

    chore: update database schema and migrations

--- prisma/migrations/20240529114605_create_model_partner/migration.sql
+++ prisma/migrations/20240529114605_create_model_partner/migration.sql
@@ -1,11 +1,5 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Partner` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "Partner";
+-- CreateExtension
+CREATE EXTENSION IF NOT EXISTS "postgis";
 
 -- CreateTable
 CREATE TABLE "partners" (
