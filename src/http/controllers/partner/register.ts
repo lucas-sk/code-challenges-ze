8f6176fd18e84f984395ad1afc34bbe3c03c0688
lusk <lucas78654321@outlook.com>
2024-05-30 10:08:05 -0300

    fix: remove comments

--- src/http/controllers/partner/register.ts
+++ src/http/controllers/partner/register.ts
@@ -1,38 +1,3 @@
-// export async function registerPartner(request: FastifyRequest, reply: FastifyReply) {
-//   const registerPartner = z.object({
-//     tradingName: z.string(),
-//     ownerName: z.string(),
-//     document: z.string(),
-//     coverageArea: z.object({
-//       type: z.string(),
-//       coordinates: z.array(z.array(z.array(z.array(z.number())))),
-//     }),
-//     address: z.object({
-//       type: z.string(),
-//       coordinates: z.array(z.number()),
-//     }),
-//   })
-//   const { tradingName, ownerName, document, coverageArea, address } =
-//     registerPartner.parse(request.body)
-//   try {
-//     const registerPartnerUseCase = makeRegisterPartnerUseCase()
-//     const partner = await registerPartnerUseCase.execute({
-//       tradingName,
-//       ownerName,
-//       document,
-//       coverageArea,
-//       address,
-//     })
-//     return reply.status(201).send(partner)
-//   } catch (err) {
-//     if (err instanceof PartnerAlreadyExistError) {
-//       return reply.status(409).send({
-//         message: err.message,
-//       })
-//     }
-//     throw err
-//   }
-// }
 import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import z from 'zod'
